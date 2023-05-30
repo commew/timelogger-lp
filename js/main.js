318bc14cdc432da3dcc829324a2347855721e5cd
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-05-31 00:13:01 +0900

    MV以外完成

--- js/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const accordions = document.querySelectorAll("dt");
-
-accordions.forEach((accordion) => {
-  accordion.addEventListener("click", () => {
-    accordion.parentNode.classList.toggle("appear");
-  });
-});
---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+//質問クリックでアンサー表示
+const accordions = document.querySelectorAll("dt");
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    accordion.parentNode.classList.toggle("appear");
+  });
+});
+
+//アンダーバーアニメーション
+const animatedBorders = document.querySelectorAll("[data-animate]");
+const animatedImages = document.querySelectorAll(".animated-img");
+
+window.addEventListener("scroll", () => {
+  const windowBottom = window.pageYOffset + window.innerHeight;
+
+  animatedBorders.forEach((el) => {
+    const elBottom = el.offsetTop + el.offsetHeight;
+
+    if (windowBottom > elBottom) {
+      el.classList.add("animate");
+    }
+  });
+
+  animatedImages.forEach((el) => {
+    const elBottom = el.offsetTop + el.offsetHeight;
+
+    if (windowBottom > elBottom) {
+      el.classList.add("animate");
+    }
+  });
+});
