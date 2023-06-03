4e29b49766fab78aa4dfaac31cb86fbf0782d591
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-06-04 00:09:16 +0900

    MVレスポンシブ対応中一旦上げ

--- public/js/main.js
+++ public/js/main.js
@@ -33,8 +33,33 @@ window.addEventListener("scroll", () => {
   });
 });
 
-document
-  .querySelector(".parallel-quadrangle-a")
-  .addEventListener("animationend", () => {
-    document.querySelector(".MV_image-main").style.opacity = "1";
+window.onload = function () {
+  // アニメーション終了を検知するためのフラグを設定
+  var animationEndFlag1 = false;
+  var animationEndFlag2 = false;
+
+  // mv-bg-img-1要素を取得します
+  var bgImage1 = document.querySelector(".mv-bg-img-1");
+  // mv-bg-img-2要素を取得します
+  var bgImage2 = document.querySelector(".mv-bg-img-2");
+
+  // mv-bg-img-1のアニメーション終了を検知するイベントリスナーを設定
+  bgImage1.addEventListener("animationend", function () {
+    animationEndFlag1 = true;
+    showMainImage();
   });
+
+  // mv-bg-img-2のアニメーション終了を検知するイベントリスナーを設定
+  bgImage2.addEventListener("animationend", function () {
+    animationEndFlag2 = true;
+    showMainImage();
+  });
+
+  function showMainImage() {
+    if (animationEndFlag1 && animationEndFlag2) {
+      // 両方のアニメーションが終了したら、.MV_image-main要素を取得して表示
+      var mainImage = document.querySelector(".MV_image-main");
+      mainImage.style.display = "block";
+    }
+  }
+};
