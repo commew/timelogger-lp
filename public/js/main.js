407318b96e3530b054206542d4a941c9c3f25fd6
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-06-04 16:15:54 +0900

    レスポンシブ対応MVの右側の色が上手く出来ない為非表示にしてます

--- public/js/main.js
+++ public/js/main.js
@@ -36,12 +36,9 @@ window.addEventListener("scroll", () => {
 window.onload = function () {
   // アニメーション終了を検知するためのフラグを設定
   var animationEndFlag1 = false;
-  var animationEndFlag2 = false;
 
   // mv-bg-img-1要素を取得します
   var bgImage1 = document.querySelector(".mv-bg-img-1");
-  // mv-bg-img-2要素を取得します
-  var bgImage2 = document.querySelector(".mv-bg-img-2");
 
   // mv-bg-img-1のアニメーション終了を検知するイベントリスナーを設定
   bgImage1.addEventListener("animationend", function () {
@@ -49,17 +46,11 @@ window.onload = function () {
     showMainImage();
   });
 
-  // mv-bg-img-2のアニメーション終了を検知するイベントリスナーを設定
-  bgImage2.addEventListener("animationend", function () {
-    animationEndFlag2 = true;
-    showMainImage();
-  });
-
   function showMainImage() {
-    if (animationEndFlag1 && animationEndFlag2) {
-      // 両方のアニメーションが終了したら、.MV_image-main要素を取得して表示
+    if (animationEndFlag1) {
+      // アニメーションが終了したら、.MV_image-main要素を取得して表示
       var mainImage = document.querySelector(".MV_image-main");
-      mainImage.style.display = "block";
+      mainImage.style.opacity = "1";
     }
   }
 };
